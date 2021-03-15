d4d9484b12d9a4e5d5e4fc75ce7e82ef4a5baa8b
ardeshirsaadat <ardeshirsaadat@gmail.com>
2021-03-15 01:00:26 -0400

    fix app name issue

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def create_app(test_config=None):
 
     @app.route('/')
     def index():
-        return jsonify({'hello this is my app live!!'})
+        return "hello this is my app live"
 
     @app.route('/actors', methods=['GET'])
     @requires_auth('get:actors')
@@ -181,4 +181,4 @@ def create_app(test_config=None):
 app = create_app()
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
